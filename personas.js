f39a6c2506ca0d62698b1c9f83a150f461df6583
Lales38 <128102923+Lales38@users.noreply.github.com>
2024-06-14 08:21:46 -0300

    Update personas.js

--- personas.js
+++ personas.js
@@ -24,7 +24,7 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const week = day * 7;
-let day_ini = new Date(2024, 3, 19, 8, 0, 0, 0);
+let day_ini = new Date(2024, 3, 26, 8, 0, 0, 0);
 let day_fin = new Date();
 
 diff = day_fin - day_ini;
