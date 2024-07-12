66c744978738a386b95aef12f90021d096b5e4cf
Lales38 <128102923+Lales38@users.noreply.github.com>
2024-07-12 14:24:16 -0300

    Update personas.js

--- personas.js
+++ personas.js
@@ -1,40 +1,35 @@
-const persona = [
-  "Iommi",
-  "Parmigiani",
-  "Bangert",
-  "Salvatierra",
-  "Nievas",
-  "Salerno",
-  "Ferrieri",
-  "Otero",
-];
 const ct = [
   '<img src="JuanPablo.png" alt="Juan Pablo"></img>',
   '<img src="Andres.png" alt="Andrés"></img>',
-  '<img src="Marcos.png" alt="Marcos"></img>',
+  '<img src="Ale.png" alt="Ale"></img>',  
   '<img src="Walter.png" alt="Walter"></img>',
   '<img src="Eduardo.png" alt="Eduardo"></img>',
-  '<img src="Ale.png" alt="Ale"></img>',
+  '<img src="Marcos.png" alt="Marcos"></img>',
   '<img src="Milton.png" alt="Milton"></img>',
   '<img src="Javier.png" alt="Javier"></img>',
+  '26/07/2024',
+  '02/08/2024',
+  '09/08/2024',
+  '16/08/2024',
+  '23/08/2024',
+  '30/08/2024',
+  '06/09/2024',
+  '13/09/2024'
 ];
-
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const week = day * 7;
-let day_ini = new Date(2024, 5, 28, 8, 0, 0, 0);
+let day_ini = new Date(2024, 6,12,8);
 let day_fin = new Date();
 
 diff = day_fin - day_ini;
 let a = Math.round(Math.floor(diff / week));
-let people = ct[a-1];
-let individuo = persona[a - 1];
-const usaDateFin = new Intl.DateTimeFormat('es-cl').format(day_fin);
 
-document.getElementById("persona").innerHTML = people;
+document.getElementById("index").innerHTML = ct[a+8];
+document.getElementById("persona").innerHTML = ct[a];
 
-/* document.getElementById("persona").innerHTML = individuo;*/
-document.getElementById("fecha").innerHTML = usaDateFin;
-
+function elegir() {
+  
+}
