c9c61b52ea45e1149533359882b8739a22b8a398
Lales38 <128102923+Lales38@users.noreply.github.com>
2024-11-05 11:07:29 -0300

    Update personas.js
    Actualización de fechas

--- personas.js
+++ personas.js
@@ -6,21 +6,21 @@ const ct = [
   '<img src="Ale.png" alt="Ale"></img>',
   '<img src="Javier.png" alt="Javier"></img>',
   '<img src="Milton.png" alt="Milton"></img>',
-  "13/09/2024",
-  "20/09/2024",
-  "27/09/2024",
-  "04/10/2024",
-  "11/10/2024",
-  "18/10/2024",
-  "25/10/2024",
-  "01/11/2024"
+  "08/11/2024",
+  "15/11/2024",
+  "22/11/2024",
+  "29/11/2024",
+  "06/12/2024",
+  "13/12/2024",
+  "20/12/2024",
+  "27/12/2024"
 ];
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const week = day * 7;
-let day_ini = new Date(2024, 8,13,8);
+let day_ini = new Date(2024, 10,1,8);
 let day_fin = new Date();
 
 diff = day_fin - day_ini;
