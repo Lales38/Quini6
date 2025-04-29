2c726d87722800551bffc94ccaf9b6d5a07a38b1
Lales38 <128102923+Lales38@users.noreply.github.com>
2025-04-29 07:45:48 -0300

    Update personas.js

--- personas.js
+++ personas.js
@@ -1,22 +1,21 @@
-const ct = [
- 
-  '<img src="Ale.png" alt="Ale"></img>',
-   '<img src="Marcos.png" alt="Marcos"></img>',   
-  '<img src="Milton.png" alt="Milton"></img>',
-  '<img src="JuanPablo.png" alt="Juan Pablo"></img>', 
-  '<img src="Edu.png" alt="Eduardo"></img>',  
-  "14/03/2025",
-  "21/03/2025",
-  "28/03/2025",
-  "05/04/2025" ,
-  "12/04/2025",
+const ct = [  
+ '<img src="Edu.png" alt="Eduardo"></img>',  
+ '<img src="Ale.png" alt="Ale"></img>',
+ '<img src="Marcos.png" alt="Marcos"></img>',   
+ '<img src="Milton.png" alt="Milton"></img>',
+ '<img src="JuanPablo.png" alt="Juan Pablo"></img>',
+  "09/05/2025",
+  "16/05/2025",
+  "23/05/2025",
+  "30/05/2025" ,
+  "06/006/2025",
 ];
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const week = day * 7;
-let day_ini = new Date(2025, 02,07,8);
+let day_ini = new Date(2025, 04,02,8);
 let day_fin = new Date();
 
 diff = day_fin - day_ini;
